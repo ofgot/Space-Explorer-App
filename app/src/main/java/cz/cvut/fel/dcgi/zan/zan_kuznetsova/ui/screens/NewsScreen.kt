aa453cb0064817fb96148a3db7adb9bb257ea274
Daria Kuznetsova <ofgot17@gmail.com>
2025-04-24 17:08:05 +0200

    cleaning code and change icons in news

--- app/src/main/java/cz/cvut/fel/dcgi/zan/zan_kuznetsova/ui/screens/NewsScreen.kt
+++ app/src/main/java/cz/cvut/fel/dcgi/zan/zan_kuznetsova/ui/screens/NewsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -23,6 +25,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -171,22 +174,25 @@ fun NewsItem(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center,
             ) {
-                IconButton(
-                    onClick = onDetailsClick
+                TextButton(
+                    onClick = onDetailsClick,
+                    colors = ButtonDefaults.textButtonColors(
+                        contentColor = Color.Black
+                    )
                 ) {
                     Icon(
                         painter = painterResource(R.drawable.book),
                         contentDescription = "See details",
                         modifier = Modifier.size(35.dp),
+                    )
 
-                        )
+                    Spacer(modifier = Modifier.width(6.dp))
+                    SingleLineText(
+                        text = "Read article",
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier.padding(start = 4.dp),
+                    )
                 }
-
-                SingleLineText(
-                    text = "Read article",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(start = 2.dp),
-                )
 
                 Checkbox(
                     checked = isSelected,
